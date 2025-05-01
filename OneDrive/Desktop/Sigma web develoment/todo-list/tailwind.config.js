567f9db03c825daf41c8615303445ecedf1f1a90
Sakshi0319998 <ss9269976@gmail.com>
2025-05-01 18:02:52 +0530

    first commit

---
+++ OneDrive/Desktop/Sigma web develoment/todo-list/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [   "./index.html", "./src/**/*.{cjs,mjs,cts,mts,js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
